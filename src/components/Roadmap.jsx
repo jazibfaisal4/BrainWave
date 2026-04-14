974e5deffff74c7cde16cc38530535c8214f0895
jazibfaisal4 <jazibfaisal4@gmail.com>
2026-04-14 20:23:42 +0500

    import TagLine issue resolved

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button";
 import Heading from "./Heading";
 import Section from "./Section";
-import Tagline from "./Tagline";
+import TagLine from "./TagLine";
 import { roadmap } from "../constants";
 import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
@@ -34,7 +34,7 @@ const Roadmap = () => (
                 </div>
                 <div className="relative z-1">
                   <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
-                    <Tagline>{item.date}</Tagline>
+                    <TagLine>{item.date}</TagLine>
 
                     <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
                       <img
@@ -44,7 +44,7 @@ const Roadmap = () => (
                         height={16}
                         alt={status}
                       />
-                      <div className="tagline">{status}</div>
+                      <div className="TagLine">{status}</div>
                     </div>
                   </div>
 
